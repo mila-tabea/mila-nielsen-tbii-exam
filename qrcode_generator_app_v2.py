cc5b3f322d07b7d4648bc2f6774c8dadc6f9ee9f
mila-tabea <milanielsen09@gmail.com>
2024-11-12 12:22:46 +0100

    Update qrcode_generator_app_v2.py

--- qrcode_generator_app_v2.py
+++ qrcode_generator_app_v2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from decode_qrcode_page import decode_qrcode_page
+from first_code import generate_qrcode
 
 st.set_page_config(
     page_title="Qr Code Generator v2")
@@ -8,7 +9,7 @@ options=['Generate QR Code', 'Decode QR Code','About me']
 page_selection= st.sidebar.selectbox("Menu", options)
 
 if page_selection == "Generate QR Code":
-    st.title("Generate QR Code")
+    generate_qrcode()
 elif page_selection == "Decode QR Code":
     decode_qrcode_page()
 elif page_selection == "About me":
